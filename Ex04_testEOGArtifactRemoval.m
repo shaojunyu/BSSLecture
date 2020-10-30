a97f6347ce2f8386c8381383d407e6aaa4629c4e
rsameni <reza.sameni@gmail.com>
2020-10-29 23:07:36 -0400

    New examples added

--- Ex04_testEOGArtifactRemoval.m
+++ Ex04_testEOGArtifactRemoval.m
@@ -30,6 +30,7 @@ end
 
 N = size(x, 1); % The number of channels
 T = size(x, 2); % The number of samples per channel
+t = (0 : T - 1)/fs;
 
 % Plot the channels
 PlotECG(x, 4, 'b', fs, 'Raw data channels');
@@ -41,21 +42,53 @@ s_jade = W_JADE * x;
 A_jade = pinv(W_JADE); % The mixing matrix
 
 % Plot the sources
-PlotECG(s_jade, 4, 'k', fs, 'Sources extracted by JADE');
+% PlotECG(s_jade, 4, 'k', fs, 'Sources extracted by JADE');
 
+% Channel denoising by JADE
 eog_channel = [2 3]; % check from the plots to visually detect the EOG
 s_jade_denoised = s_jade;
 s_jade_denoised(eog_channel, :) = 0;
 x_denoised_jade = A_jade * s_jade_denoised;
 
+% Channel denoising by NSCA
+eog_channel = 24;
+eog_ref = x(eog_channel, :);
+energy_envelope_len = round(0.25*fs);
+eog_envelope = sqrt(filtfilt(ones(1, energy_envelope_len), energy_envelope_len, eog_ref.^2));
+med = median(eog_envelope);
+mx = max(eog_envelope);
+eog_detection_threshold = 0.95 * med + 0.05 * mx;
+
+J = eog_envelope >= eog_detection_threshold;
+I = 1 : T;
+
+[s_nsca, W_nsca, A_nsca] = NSCA(x,J, I);
+% Channel denoising by JADE
+eog_channel = [1 2]; % check from the plots to visually detect the EOG
+s_nsca_denoised = s_nsca;
+s_nsca_denoised(eog_channel, :) = 0;
+x_denoised_nsca = A_nsca * s_nsca_denoised;
+% PlotECG(s_nsca, 4, 'r', fs, 'Sources extracted by NSCA');
+
+figure
+hold on
+plot(t, eog_ref);
+plot(t, eog_envelope);
+plot(t, eog_detection_threshold(ones(1, T)));
+grid
+legend('EOG reference channel', 'EOG envelope');
+
 % Plot the denoised signals
-t = (0 : T - 1)/fs;
 for ch = 1 : N
     figure
     hold on
     plot(t, x(ch, :));
     plot(t, x_denoised_jade(ch, :));
-    legend(['channel ' num2str(ch)], 'denoised');
+    plot(t, x_denoised_nsca(ch, :));
+    legend(['channel ' num2str(ch)], 'denoised by JADE', 'denoised by NSCA');
     grid
 end
+
+% Run the following script from the OSET package for a more advanced method
+testEOGRemoval
 
